48fc89549fbfa56ef9752680321d32627744e72c
ajayygurjar <167853061+ajayygurjar@users.noreply.github.com>
2025-07-16 14:08:59 +0530

    dynamic toggle bar header

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,47 @@
-import { createBrowserRouter ,RouterProvider } from "react-router-dom"
-import RootLayout from "./components/Layout/RootLayout"
-import HomePage from "./components/Home/HomePage"
-import AuthPage from "./components/Auth/AuthPage"
-import Dashboard from "./components/Pages/Dashboard"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/Layout/RootLayout";
+import HomePage from "./components/Home/HomePage";
+import AuthPage from "./components/Auth/AuthPage";
+import Dashboard from "./components/Pages/Dashboard";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { rehydrateUser } from "../store/authSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
 
-  
+  useEffect(() => {
+    const savedAuth = localStorage.getItem("auth");
+    if (savedAuth) {
+      dispatch(rehydrateUser(JSON.parse(savedAuth)));
+    }
+  }, [dispatch]);
 
-
-
-  const router=createBrowserRouter([
+  const router = createBrowserRouter([
     {
-       path: "/",
-    element: <RootLayout />, 
-    children:[
-      {
-        index: true,           
-        element: <HomePage />,
-      },
-      {
-        path: "auth",          
-        element: <AuthPage />,
-      },
-      {
-        path:'dashboard',
-        element:<Dashboard/>
-      }
-
-    ]
-  }
-  ])
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "auth",
+          element: <AuthPage />,
+        },
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-
-  <RouterProvider router={router}/>  
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,42 +1,52 @@
-
-import { Container, Nav, Dropdown, Navbar } from 'react-bootstrap'
-import { logout } from '../../../store/authSlice'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { Container, Nav, Dropdown, Navbar } from "react-bootstrap";
+import { logout } from "../../../store/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 const Header = () => {
-    const dispatch=useDispatch();
-    const navigate=useNavigate();
-    const handleLogout=()=>{
-        dispatch(logout());
-        navigate('/');
-    }
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
   return (
     <>
-      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand as={Link} to='/'>ChroniCare</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            ChroniCare
+          </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Dropdown align='end'>
-                <Dropdown.Toggle variant='secondary' id='dropdown-user'>
-                  User
-                </Dropdown.Toggle>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              {token ? (
+                <Dropdown align="end">
+                  <Dropdown.Toggle variant="secondary" id="dropdown-user">
+                    User
+                  </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item as={Link} to='/dashboard'>Dashboard</Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+                  <Dropdown.Menu>
+                    <Dropdown.Item as={Link} to="/dashboard">
+                      Dashboard
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : (
+                <Nav.Link as={Link} to="/auth">
+                  Login
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
